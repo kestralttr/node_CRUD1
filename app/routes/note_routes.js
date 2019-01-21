5b28d17ca6565f029174f592fd7eb1acc5ac21df
Alex Bennett <kestralttr@gmail.com>
2019-01-21 12:43:36 -0700

    adds and enables POST and GET routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,8 +1,29 @@
 
 
 module.exports = function(app, db) {
+
+    var ObjectID = require('mongodb').ObjectID;
+
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').findOne(details, (err, item) => {
+        if (err) {
+            res.send({'error':'An error has occurred'});
+        } else {
+            res.send(item);
+        }
+        });
+    });
+
     app.post('/notes', (req, res) => {
-        // You'll create your note here.
-        res.send('Hello')
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+        if (err) { 
+            res.send({ 'error': 'An error has occurred' }); 
+        } else {
+            res.send(result.ops[0]);
+        }
+        });
     });
 };
